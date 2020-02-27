01cfc0d9d4b7e3080aa6689d4a9bfa066a92d027
ahmadkhaleel96 <runner@fv-az71.gf1u5lkn5mfe5lr0l3zmewle2e.gx.internal.cloudapp.net>
2020-02-27 14:08:35 +0000

    Updates

---
+++ precache-manifest.405162cdafef7d5f39790245044c1f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98155edc77754383f63511ec42759556",
+    "url": "/react-testinganddeployment/index.html"
+  },
+  {
+    "revision": "0c3cd3b72e7cd86ec263",
+    "url": "/react-testinganddeployment/static/css/main.240be3e0.chunk.css"
+  },
+  {
+    "revision": "f6e3eefa053bc5d9b767",
+    "url": "/react-testinganddeployment/static/js/2.d72b5e9f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-testinganddeployment/static/js/2.d72b5e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3cd3b72e7cd86ec263",
+    "url": "/react-testinganddeployment/static/js/main.37127d6f.chunk.js"
+  },
+  {
+    "revision": "69ac007e336bb6cea775",
+    "url": "/react-testinganddeployment/static/js/runtime-main.0daaf688.js"
+  }
+]);
