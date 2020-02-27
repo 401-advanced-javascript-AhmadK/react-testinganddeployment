12d0d3da06f897b4f74ca8b05657d14227f71111
ahmadkhaleel96 <runner@fv-az83.itcen245tbsulenlxhsfn1fmvc.bx.internal.cloudapp.net>
2020-02-27 14:27:16 +0000

    Updates

---
+++ precache-manifest.7862d9c5cf5d1cde37e05116fc91442a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc35d452099dfacab292e0f729189334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3cd3b72e7cd86ec263",
+    "url": "/static/css/main.240be3e0.chunk.css"
+  },
+  {
+    "revision": "f6e3eefa053bc5d9b767",
+    "url": "/static/js/2.d72b5e9f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d72b5e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3cd3b72e7cd86ec263",
+    "url": "/static/js/main.37127d6f.chunk.js"
+  },
+  {
+    "revision": "b3183914cc1a436fc768",
+    "url": "/static/js/runtime-main.0b4940b4.js"
+  }
+]);
